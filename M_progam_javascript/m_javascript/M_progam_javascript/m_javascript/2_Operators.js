37b1f7df48ce6f38049b59714973fb1ae5eafdbf
Oninakar <101468132+Oninakar@users.noreply.github.com>
2022-08-30 18:52:58 +0630

    Add files via upload

---
+++ M_progam_javascript/m_javascript/M_progam_javascript/m_javascript/2_Operators.js
@@ -0,0 +1,150 @@
+//(Operators)
+
+
+//(Arithmetic operators)
+
+// +(Addion)
+
+/*  let num1 = 2;
+    let num2 = 3;
+    let num3 = num1 + num2; // num3 = 5;
+
+    let str1 = "hello";
+    let str2 = "world";
+    let str3 = str1 + str2; // str3 = "hellworld";
+    let str3 = str1 + "" + str2; // str3 = "hello world"
+    (string conecatenation)
+
+
+// -(subration)
+
+    let num1 = 2;
+    let num2 = 3;
+    let num3 = num1 - num2; // num3 = -1;
+
+
+// *(Mulipication)
+
+    let num1 = 2;
+    let num2 = 5;
+    let num3 = num1 * num2; // num3 = 10;
+
+
+// /(division)
+
+    let num1 = 6;
+    let num2 = 3;
+    let num3 = num1 / num1;// num3 = 2;
+
+
+// %(Remainder)
+
+    let num1 = 11;
+    let num2 = 3;
+    let num3 = num1 % num2;// num3 = 2;
+
+
+// **(Exponet)
+
+    let num1 = 5;
+    let num2 = 5;
+    let num3 = num1 ** num2;// num3 = 25;
+
+
+// (Increment and decrement Operators)
+
+// (++)
+
+    let num1 = 1;
+    let num2 = num1 ++ // num 2 = 2;
+
+
+// (--)
+
+    let num1 = 1;
+    let num2 = num1 -- // num2 = 0;
+
+
+// (Assignment Operators)
+
+
+// =+ (Addition assignment)
+
+    let num1 = 1;
+    num1 += 5 // num1 = 6;
+
+
+// -= (Subtraction assignment)
+
+    let num1 = 5;
+    num1 -= 5; // num = 5;
+
+
+// *= (Multiplication assignment)
+
+    let num1 = 5;
+    num1 *= 2; // num1 = 10;
+
+
+// /= (Division assignment)
+
+    let num1 = 10;
+    num1 /= 2; // num1 = 5;
+
+
+
+// (Comparison Operators)
+
+// == vs ===
+
+    let num = 5;
+    num == "5"; // output true (too tat type ko pyung py pr tl)
+    num === "5"; // output false (data type ma pyung py pr buu)
+
+
+// != vs !==
+
+    let num = 5;
+    num != "5"; // output false
+    num !== "5"; // output true
+
+
+// > (greater than) and (less than)
+
+    let num1 = 2;
+    let num2 = 3;
+    num 1 > num2; output false
+    num 1 < num2; output true
+
+
+// >= (greater than or equal to) and <= (less than or equal to)
+
+    let num1 = 2;
+    let num2 = 3;
+    num1 >= num2; output false
+    num1 <= num2; output true
+
+
+// (Logical Operator)
+
+
+// && (and logical operator)
+
+    num1 > 3 && num2 < 10; // false
+    num1 < 7 && num2 > 5; // true
+
+
+// || (or logical operator)
+
+    num1 > 3 || num2 < 10; // true
+    num1 <7 || num2 > 5; // false
+    num1 >= 5 num2 <= 10; // true
+
+
+// ! (not logical operator)
+
+    ! (num1 === num2) // true
+    ! (num1 < num2) // false
+
+    */
+

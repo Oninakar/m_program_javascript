da323e09cfa3ebebd4ee15a5d566ffca0bb17610
Oninakar <101468132+Oninakar@users.noreply.github.com>
2022-08-31 23:12:13 +0630

    Add files via upload

---
+++ m_javascript/8_object_2.js
@@ -0,0 +1,36 @@
+// object (part 2)
+
+
+const person1 ={
+    name: "Aung Paine", // key value pair
+    age: 50,
+    height: 180,
+    city: "Yangon",
+    dateOfBirthday: 1970,
+    isActor: true,
+    walk: () => {
+        alert ("Aung Paine is walking now..")
+    }
+}
+person1. hobby = "football";//<-- person1 object mhar assing lote py pr tl
+person1. hobby = "chin_lone";//<-- person1 hobby football nya yr mhar chin_lone hobby change twr pr tl
+person1. isMale = true //<-- Bollen ll use lo ya pr tl
+person1. eat =  () => {     //<-- math tway ll use lo ya pr tl
+    alert ("Aung is eating now..")
+}
+
+const input1Value = "";
+const input2Value = "";
+const propertyName = "donatedBy" + input1Value;
+// dinatedByMgMg: 10000;
+person1[propertyName] = input2Value;
+
+/*
+chrome network request --> server
+server --> return ovbject
+*/
+
+
+// window object
+alert ("hello")
+console. logL("welcome")
